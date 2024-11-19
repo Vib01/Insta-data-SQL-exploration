cf9f72c13c7394cc5ad6591e3dfa2c6613a3bfc1
Vibhu Sharma <109180141+Vib01@users.noreply.github.com>
2024-11-19 15:10:07 -0330

    Add files via upload

---
+++ Insta data exploring query.sql
@@ -0,0 +1,19 @@
+SELECT id,username,category, followers
+FROM users01
+ORDER BY followers DESC
+LIMIT 10;
+---------------------
+SELECT id,username,avg_likes
+FROM users01
+ORDER BY avg_likes DESC
+LIMIT 10;
+----------------------
+SELECT id, username, channel_info, category, followers
+FROM (
+    SELECT id, username, channel_info, category, followers,
+           ROW_NUMBER() OVER (PARTITION BY category ORDER BY followers DESC) AS row_num
+    FROM users01
+) AS ranked
+WHERE row_num = 1
+ORDER BY followers DESC
+LIMIT 15;
